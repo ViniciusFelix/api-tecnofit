6052b89b784056a5f7f769f2a73f2a47cf76597d
Vinícius Félix da Silva <vinicius.felix.silva@gmail.com>
2022-02-21 11:00:31 -0300

    Add files via upload

---
+++ database/factories/RankingFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RankingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name()
+        ];
+    }
+    
+    /**
+     * Retorno do recorde pelo usuario e movimento.
+     * 
+     * @param integer $idMoviment
+     * @param integer $idUser
+     * @return array
+     */
+    public function getRecordUser($idMoviment,$idUser)
+    {
+        $sql = "
+            select 
+            	pr.value as record,
+                pr.`date` as data,
+                m.name as movimento
+            from tecnofit.personal_record pr 
+            join tecnofit.moviments m 
+              on m.id = pr.movement_id 
+            where pr.value = (
+            select max(pr2.value)
+             from tecnofit.personal_record pr2 
+             where pr2.user_id = {$idUser}
+               and pr2.movement_id = {$idMoviment})
+            and pr.user_id = {$idUser}
+            and pr.movement_id = {$idMoviment}
+            order by pr.`date` desc
+        ";
+        
+        return \DB::select($sql);
+    }
+}
